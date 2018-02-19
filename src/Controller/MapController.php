6b468c0a17056d193eec740eccde4b0548c4e8d8
Fayjus Salehin <fsalehin@live.com>
2018-02-19 21:27:59 +0200

    route to home page working

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class MapController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('OMG! the map page!');
